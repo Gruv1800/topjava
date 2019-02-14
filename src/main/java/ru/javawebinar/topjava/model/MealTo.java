912d288716f8bfaac2599eb985155c6fd70d342f
Gruv <gruv1800@gmail.com>
2019-02-14 23:23:14 +0300

    Apply patch 2_1_HW_1

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -17,4 +17,20 @@ public class MealTo {
         this.calories = calories;
         this.excess = excess;
     }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public boolean isExcess() {
+        return excess;
+    }
 }
