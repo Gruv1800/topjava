843d1c4cbf8b04841d5ac9ef5b3dd70e67deeb48
gruzdev <gruzdev@merann.ru>
2019-02-27 11:31:38 +0300

    DB's scripts was created.

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -9,3 +10,8 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (description, calories) VALUES
+  ('Lunch', 3000),
+  ('Dinner', 1500),
+  ('Breakfast', 2000);
