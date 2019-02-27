3af91df90e619efa522ec1bbd9f6961ba1bc9591
Gruv <gruv1800@gmail.com>
2019-02-27 23:18:56 +0300

    HW_03 Correct DB structure.

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,6 @@
 DELETE FROM user_roles;
-DELETE FROM users;
 DELETE FROM meals;
+DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -11,7 +11,7 @@ INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
 
-INSERT INTO meals (description, calories) VALUES
-  ('Lunch', 3000),
-  ('Dinner', 1500),
-  ('Breakfast', 2000);
+INSERT INTO meals (user_id, description, calories) VALUES
+  (100000, 'Lunch', 3000),
+  (100000, 'Dinner', 1500),
+  (100001, 'Breakfast', 2000);
